75a51d6c5d5ccaf132f8eefd7e65fbeede30424f
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-31 01:39:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, data|
     data.each do |holiday, supply|
-      if holiday == 
+      if holiday == :christmas
 end
 
 
